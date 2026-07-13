a7892819a95773de4bf497c57b6529df4799cc67
ciciliareghina21 <ciciliareghina21@gmail.com>
2026-07-13 10:36:59 +0800

    Tugas 2 Cloud

---
+++ app/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tugas Docker</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>TUGAS DOCKER</h1>
+
+    <h2>Aplikasi Biodata Mahasiswa</h2>
+
+    <table>
+        <tr>
+            <td>Nama</td>
+            <td>: Reghina Cicilia</td>
+        </tr>
+
+        <tr>
+            <td>NIM</td>
+            <td>: 2410010300</td>
+        </tr>
+
+        <tr>
+            <td>Program Studi</td>
+            <td>: Teknik Informatika</td>
+        </tr>
+
+        <tr>
+            <td>Semester</td>
+            <td>: 4</td>
+        </tr>
+    </table>
+
+    <p>
+        Aplikasi ini berjalan menggunakan Docker Container.
+    </p>
+
+</div>
+
+</body>
+</html>
